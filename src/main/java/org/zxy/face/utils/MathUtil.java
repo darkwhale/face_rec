6995e2e3bcf1237e6cf97702590fbd6586966e08
zxy <zxy@up.com>
2020-05-26 22:39:48 +0800

    1. BigDecimal改为Integer,一般不会有那么多调用次数；

--- src/main/java/org/zxy/face/utils/MathUtil.java
+++ src/main/java/org/zxy/face/utils/MathUtil.java
@@ -1,13 +1,11 @@
 package org.zxy.face.utils;
 
-import java.math.BigDecimal;
-
 public class MathUtil {
 
     public static String add(String numStr){
         if (numStr.equals("")) {
             return "1";
         }
-        return String.valueOf(new BigDecimal(numStr).add(BigDecimal.ONE));
+        return String.valueOf(Integer.parseInt(numStr) + 1);
     }
 }
